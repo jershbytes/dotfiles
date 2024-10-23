287109236b6f1853a2ac95d96c06741bfecfa91d
Joshua Ross <josh@rossjm.dev>
2024-10-23 09:17:03 -0400

    updated wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -12,6 +13,16 @@ config.enable_tab_bar = false
 
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
+
+
+config.mouse_bindings = {
+
+    {
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'CTRL',
+        action = act.OpenLinkAtMouseCursor,
+      }
+    }
 
 -- and finally, return the configuration to wezterm
 return config
