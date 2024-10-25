f8d3db7d22a6d8fda890ae532848b03d942c4260
Joshua Ross <josh@rossjm.dev>
2024-10-24 22:15:46 -0400

    made some changes to wezterm and aliases

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,7 +6,6 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-
 config.color_scheme = "Dracula"
 
 config.enable_tab_bar = false
@@ -14,16 +13,28 @@ config.enable_tab_bar = false
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
-
+-- Mouse bindings
 config.mouse_bindings = {
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = act.OpenLinkAtMouseCursor,
+  }
+}
 
-    {
-        event = { Up = { streak = 1, button = 'Left' } },
-        mods = 'CTRL',
-        action = act.OpenLinkAtMouseCursor,
-      }
-    }
+-- Key bindings for switching between tabs
+config.keys = {
+  {
+    key = "LeftArrow",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    key = "RightArrow",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+}
 
--- and finally, return the configuration to wezterm
+-- And finally, return the configuration to wezterm
 return config
-
