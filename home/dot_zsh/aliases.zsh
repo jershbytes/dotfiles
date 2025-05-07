36cf274470d139cc695cf743b886fe2840f59cf4
jershbytes <git@rossjm.dev>
2025-05-07 11:37:04 -0400

    Changed Aliases

--- home/dot_zsh/aliases.zsh
+++ home/dot_zsh/aliases.zsh
@@ -1,6 +1,6 @@
 # Terminal Management Aliases
 alias c="clear"                           # Quickly clear the terminal screen
-alias cd="z"                              # Jump to directories using z (tracking frequently used paths)
+alias zz="z"                              # Jump to directories using z (tracking frequently used paths)
 alias lt='ls --human-readable --size -1 -S --classify'  # List files by size (largest first) with human-readable sizes and file types
 alias ll='ls -alF --color=auto'                        # List all files (including hidden) in long format with indicators for file types
 alias la='ls -A --color=auto'                          # Show all files except `.` and `..` (almost all)
