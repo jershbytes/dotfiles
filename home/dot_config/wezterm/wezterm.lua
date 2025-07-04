c82540b4ed7a48e41675c05aa73acad44e533b2b
jershbytes <git@rossjm.dev>
2025-07-04 16:56:05 -0400

    Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- Color Scheme
-config.color_scheme = 'Monokai Pro Spectrum'
+config.color_scheme = 'Dracula'
 
 -- Font
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
