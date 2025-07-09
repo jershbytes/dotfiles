3491ad9c9d111dd0c8139e8de93e91ec561f4e4c
Joshua Ross <git@rossjm.dev>
2025-07-09 12:30:37 -0400

    Update home/.config/nvim/lua/plugins/alpha.lua

--- home/dot_config/nvim/lua/plugins/alpha.lua
+++ home/dot_config/nvim/lua/plugins/alpha.lua
@@ -1,11 +1,12 @@
 return {
   "goolord/alpha-nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional, for icons
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    vim.cmd.colorscheme "monokai"
-    vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#f92672", bold = true })
-    vim.api.nvim_set_hl(0, "AlphaButtons", { fg = "#a6e22e" })
-    vim.api.nvim_set_hl(0, "AlphaFooter", { fg = "#66d9ef" })
+    -- Custom highlights (Monokai Pro colors)
+    vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#f92672", bold = true })    -- Pink
+    vim.api.nvim_set_hl(0, "AlphaButtons", { fg = "#a6e22e" })                -- Green
+    vim.api.nvim_set_hl(0, "AlphaFooter", { fg = "#66d9ef" })                 -- Cyan
+
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
 
@@ -22,13 +23,13 @@ return {
 
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "  > Find file", ":cd $HOME/Projects | Telescope find_files<CR>"),
+      dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
       dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
       dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
       dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
     }
 
-    -- Optional: If you want a custom footer, uncomment and customize below
+    -- Uncomment for a custom footer
     -- dashboard.section.footer.val = { "Welcome to Neovim!" }
 
     alpha.setup(dashboard.opts)
