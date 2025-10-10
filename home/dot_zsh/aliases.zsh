395a5e992a5301f63db42e9d32ea69feb83be135
Joshua Ross <git@rossjm.dev>
2025-10-10 14:57:41 -0400

    Promot and zsh changes

--- home/dot_zsh/aliases.zsh
+++ home/dot_zsh/aliases.zsh
@@ -1,23 +1,111 @@
-# Terminal Management Aliases
-alias zz="z"                              # Jump to directories using z (tracking frequently used paths)
-alias cat="bat"                            # Use bat instead of cat for syntax highlighting
-alias lt='ls --human-readable --size -1 -S --classify'  # List files by size (largest first) with human-readable sizes and file types
-alias ll='ls -alF --color=auto'                        # List all files (including hidden) in long format with indicators for file types
-alias la='ls -A --color=auto'                          # Show all files except `.` and `..` (almost all)
-alias ls='ls -l --color=auto'                          # Display files in detailed list format by default
-alias l='ls -CF --color=auto'                          # List files in columns and append file type indicators
-alias bye='shutdown -h now'               # Shut down the machine (if you know, you know)
-alias ff='fastfetch'                      # Run fastfetch (a system information tool)
-
-# File Operations Aliases
-alias cpv='rsync -ah --info=progress2'    # Copy files using rsync with progress info (human-readable)
-
-# Development Tools Aliases                     # Shortcut for Terraform commands
-alias vim="nvim"
-
-# Directory Tree Display Alias
-alias tree='tree --dirsfirst -F'          # Display directory tree with directories listed first and trailing slashes
-
 # Chezmoi Aliases
 alias dot="chezmoi edit"                       # Alias for Chezmoi edit command
 alias dotdrop="chezmoi update && chezmoi apply"                # Serve MkDocs documentation locally
+
+# Alias's to modified commands
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='trash -v'
+alias mkdir='mkdir -p'
+alias ps='ps auxf'
+alias ping='ping -c 10'
+alias less='less -R'
+alias cls='clear'
+alias apt-get='sudo apt-get'
+alias multitail='multitail --no-repeat -c'
+alias freshclam='sudo freshclam'
+alias vi='nvim'
+alias svi='sudo vi'
+alias vis='nvim "+set si"'
+alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
+
+# Change directory aliases
+alias home='cd ~'
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+# Remove a directory and all files
+alias rmd='/bin/rm  --recursive --force --verbose '
+
+# Alias's for multiple directory listing commands
+alias la='ls -Alh'                # show hidden files
+alias ls='ls -aFh --color=always' # add colors and file type extensions
+alias lx='ls -lXBh'               # sort by extension
+alias lk='ls -lSrh'               # sort by size
+alias lc='ls -ltcrh'              # sort by change time
+alias lu='ls -lturh'              # sort by access time
+alias lr='ls -lRh'                # recursive ls
+alias lt='ls -ltrh'               # sort by date
+alias lm='ls -alh |more'          # pipe through 'more'
+alias lw='ls -xAh'                # wide listing format
+alias ll='ls -Fls'                # long listing format
+alias labc='ls -lap'              # alphabetical sort
+alias lf="ls -l | egrep -v '^d'"  # files only
+alias ldir="ls -l | egrep '^d'"   # directories only
+alias lla='ls -Al'                # List and Hidden Files
+alias las='ls -A'                 # Hidden Files
+alias lls='ls -l'                 # List
+
+# alias chmod commands
+alias mx='chmod a+x'
+alias 000='chmod -R 000'
+alias 644='chmod -R 644'
+alias 666='chmod -R 666'
+alias 755='chmod -R 755'
+alias 777='chmod -R 777'
+
+# Search command line history
+alias h="history | grep "
+
+# Search running processes
+alias p="ps aux | grep "
+alias topcpu="/bin/ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10"
+
+# Search files in the current folder
+alias f="find . | grep "
+
+# Count all files (recursively) in the current folder
+alias countfiles="for t in files links directories; do echo \`find . -type \${t:0:1} | wc -l\` \$t; done 2> /dev/null"
+
+# To see if a command is aliased, a file, or a built-in command
+alias checkcommand="type -t"
+
+# Show open ports
+alias openports='netstat -nape --inet'
+
+# Alias's for safe and forced reboots
+alias rebootsafe='sudo shutdown -r now'
+alias rebootforce='sudo shutdown -r -n now'
+
+# Alias's to show disk space and space used in a folder
+alias diskspace="du -S | sort -n -r |more"
+alias folders='du -h --max-depth=1'
+alias folderssort='find . -maxdepth 1 -type d -print0 | xargs -0 du -sk | sort -rn'
+alias tree='tree -CAhF --dirsfirst'
+alias treed='tree -CAFd'
+alias mountedinfo='df -hT'
+
+# Alias's for archives
+alias mktar='tar -cvf'
+alias mkbz2='tar -cvjf'
+alias mkgz='tar -cvzf'
+alias untar='tar -xvf'
+alias unbz2='tar -xvjf'
+alias ungz='tar -xvzf'
+
+# Show all logs in /var/log
+alias logs="sudo find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -f1 | sed -e's/:$//g' | grep -v '[0-9]$' | xargs tail -f"
+
+# SHA1
+alias sha1='openssl sha1'
+
+alias clickpaste='sleep 3; xdotool type "$(xclip -o -selection clipboard)"'
+
+alias docker-clean=' \
+  docker container prune -f ; \
+  docker image prune -f ; \
+  docker network prune -f ; \
+  docker volume prune -f '
