542ecf04ebc1b16eb79907c312ecf2ce43257788
Joshua Ross <git@rossjm.dev>
2025-12-30 13:57:35 -0500

    Enhance executable_git-remote-link.sh for Azure DevOps
    Added support for Azure DevOps SSH URLs in the script.

--- home/dot_config/starship/executable_git-remote-link.sh
+++ home/dot_config/starship/executable_git-remote-link.sh
@@ -16,13 +16,20 @@ elif [[ "$GIT_REMOTE" =~ "codeberg" ]]; then
     ICON=$'\uf330'  # Codeberg icon
 elif [[ "$GIT_REMOTE" =~ "bitbucket" ]]; then
     ICON=$'\uf171'  # Bitbucket icon
+elif [[ "$GIT_REMOTE" =~ "dev.azure.com" ]] || [[ "$GIT_REMOTE" =~ "visualstudio.com" ]]; then
+    ICON=$'\uebd8'  # Azure DevOps icon
 else
     ICON=$'\uf1d3'  # Generic git icon
 fi
 
 # Convert SSH URLs to HTTPS
-# Handles: git@github.com:user/repo.git -> https://github.com/user/repo
-url=$(echo "$GIT_REMOTE" | sed -E 's|git@([^:]+):(.+)\.git|https://\1/\2|')
+# Handle Azure DevOps SSH: git@ssh.dev.azure. com:v3/org/project/repo -> https://dev.azure.com/org/project/_git/repo
+if [[ "$GIT_REMOTE" =~ git@ssh\.dev\.azure\.com:v3/([^/]+)/([^/]+)/(.+) ]]; then
+    url="https://dev.azure.com/${BASH_REMATCH[1]}/${BASH_REMATCH[2]}/_git/${BASH_REMATCH[3]}"
+else
+    # Standard SSH: git@github.com:user/repo.git -> https://github.com/user/repo
+    url=$(echo "$GIT_REMOTE" | sed -E 's|git@([^: ]+):(.+)\.git|https://\1/\2|')
+fi
 
 # Also handle HTTPS URLs and remove .git suffix
 url=$(echo "$url" | sed 's|\.git$||')
@@ -32,4 +39,4 @@ repo=$(basename "$url")
 
 # Output: icon + two spaces + OSC 8 hyperlink
 # OSC 8 format: ESC]8;;URL\ESC\\TEXT\ESC]8;;\ESC\\
-printf '%s \e]8;;%s\e\\%s\e]8;;\e\\' "$ICON" "$url" "$repo"
+printf '%s  \e]8;;%s\e\\%s\e]8;;\e\\' "$ICON" "$url" "$repo"
