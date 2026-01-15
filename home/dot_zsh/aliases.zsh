ff692b13e6812e2a379731c1d308ab3a09448a1a
Joshua Ross <git@rossjm.dev>
2026-01-15 00:34:35 -0500

    Update home/.zsh/aliases.zsh

--- home/dot_zsh/aliases.zsh
+++ home/dot_zsh/aliases.zsh
@@ -12,6 +12,7 @@ alias vim='nvim'
 alias home='cd ~'
 alias cd="z"
 alias ls="eza --icons=always"
+alias code="vscodium"
 
 # Remove a directory and all files
 alias rmd='/bin/rm  --recursive --force --verbose '
