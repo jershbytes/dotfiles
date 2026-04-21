a870181ddaf0da7b53eba8e63e2d4a8dcd47643b
JershBytes <git@rossjm.dev>
2026-04-21 14:12:08 -0400

    Initial commit

---
+++ home/dot_zsh/aliases.zsh
@@ -0,0 +1,57 @@
+# Alias's to modified commands
+alias cat='bat'
+alias cp='cp -i'
+alias mv='mv -i'
+alias mkdir='mkdir -p'
+alias ps='ps auxf'
+alias ping='ping -c 10'
+alias less='less -R'
+alias home='cd ~'
+alias cd="z"
+alias ls='eza --icons --color=always'
+alias apt='nala'
+
+# Remove a directory and all files
+alias rmd='/bin/rm  --recursive --force --verbose '
+
+# Search command line history
+alias huh="history | grep "
+
+# Search running processes
+alias p="ps aux | grep "
+alias topcpu="/bin/ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10"
+
+# Search files in the current folder
+alias f="find . | grep "
+
+# Count all files (recursively) in the current folder
+alias countfiles="for t in files links directories; do echo \`find . -type \${t:0:1} | wc -l\` \$t; done 2> /dev/null"
+
+# To see if a command is aliased, a file, or a built-in command
+alias checkcommand="type -t"
+
+# Alias's for archives
+alias mktar='tar -cvf'
+alias mkbz2='tar -cvjf'
+alias mkgz='tar -cvzf'
+alias untar='tar -xvf'
+alias unbz2='tar -xvjf'
+alias ungz='tar -xvzf'
+
+# Show all logs in /var/log
+alias logs="sudo find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -f1 | sed -e's/:$//g' | grep -v '[0-9]$' | xargs tail -f"
+
+# SHA1
+alias sha1='openssl sha1'
+
+# fastfetch Aliases
+alias ff='fastfetch'
+
+# chezmoi alias
+alias cz='chezmoi'
+alias cza='chezmoi add'
+alias czcd='chezmoi cd'
+alias cze='chezmoi edit'
+alias czea='chezmoi edit --apply'
+alias czra='chezmoi re-add'
+alias czu='chezmoi update'
