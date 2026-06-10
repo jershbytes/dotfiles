135deb6de9abf0b75774f126670badb7d2447f70
JershBytes <git@rossjm.dev>
2026-06-10 09:20:47 -0400

    Update .chezmoiscripts/common/install-prompt.sh

--- home/.chezmoiscripts/common/run_onchange_before_install-prompt.sh
+++ home/.chezmoiscripts/common/run_onchange_before_install-prompt.sh
@@ -19,6 +19,16 @@ else
   echo "zoxide is already installed: $(zoxide --version)"
 fi
 
+if ! command -v yazi &>/dev/null; then
+  echo "yazi not found. Installing..."
+curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | gpg --dearmor --yes | sudo tee /etc/apt/trusted.gpg.d/debian.griffo.io.gpg > /dev/null
+echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | sudo tee /etc/apt/sources.list.d/debian.griffo.io.list
+sudo apt update
+sudo apt install yazi
+else
+  echo "yazi is already installed: $(yazi --version)"
+fi
+
 # Ubuntu installs bat and fd under different names — symlink them so everything works
 [ -L ~/.local/bin/bat ] || ln -s "$(which batcat)" ~/.local/bin/bat
 [ -L ~/.local/bin/fd ] || ln -s "$(which fdfind)" ~/.local/bin/fd
