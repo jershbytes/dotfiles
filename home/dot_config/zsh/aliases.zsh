a4cee9b482b710217faede5ad0579064620318c6
Joshua Ross <git@rossjm.dev>
2026-06-10 00:29:28 -0400

    Update .config/zsh/aliases.zsh Update .config/zsh/.zshrc

--- home/dot_config/zsh/aliases.zsh
+++ home/dot_config/zsh/aliases.zsh
@@ -1,24 +1,18 @@
 #!/usr/bin/env zsh
 
-# Alias's to modified commands
-alias cat='bat'
+# Navigation
+alias cd='z'
+alias tree='eza --tree --icons'
+alias ll='eza -lh --icons --git'
+alias la='eza -lah --icons --git'
+
+# File operations
 alias cp='cp -i'
 alias mv='mv -i'
 alias mkdir='mkdir -p'
-alias ps='ps auxf'
-alias ping='ping -c 10'
-alias less='less -R'
-alias cls='clear'
-alias vi='neovide'
-alias vim='neovide'
-alias cd="z"
-alias ls='eza --icons --color=always'
-alias rp='rpaste'
+alias rmd='/bin/rm --recursive --force --verbose'
 
-# Remove a directory and all files
-alias rmd='/bin/rm  --recursive --force --verbose '
-
-# Alias's for archives
+# Archives
 alias mktar='tar -cvf'
 alias mkbz2='tar -cvjf'
 alias mkgz='tar -cvzf'
@@ -26,13 +20,25 @@ alias untar='tar -xvf'
 alias unbz2='tar -xvjf'
 alias ungz='tar -xvzf'
 
-# General useful alias's
-alias bye="shutdown -h now"
+# Editors
+alias vi='nvim'
+alias vim='nvim'
 
-# fastfetch Aliases
+# Viewing / output
+alias cat='bat'
+alias less='less -R'
+alias cls='clear'
+
+# System
+alias ps='ps auxf'
+alias ping='ping -c 10'
+alias bye='shutdown -h now'
+alias flush-dns='sudo systemd-resolve --flush-caches'
+
+# Tools
 alias ff='fastfetch'
 
-# chezmoi alias
+# chezmoi
 alias cz='chezmoi'
 alias cza='chezmoi add'
 alias czcd='chezmoi cd'
@@ -41,5 +47,5 @@ alias czea='chezmoi edit --apply'
 alias czra='chezmoi re-add'
 alias czu='chezmoi update'
 
-# Systemd Aliases
-alias flush-dns='sudo systemd-resolve --flush-caches'
+# Reuse ls completions for eza (avoids defining a separate completion function)
+compdef eza=ls
