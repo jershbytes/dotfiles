5c8e258c8e12bd9e029289a11b5aa5f4cdb9441d
Joshua Ross <git@rossjm.dev>
2026-06-19 01:05:36 -0400

    Update .config/nvim/lua/plugins/colorscheme.lua

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,83 +1,169 @@
 return {
   {
-    "loctvl842/monokai-pro.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("monokai-pro")
-    end,
-    keys = { { "<leader>C", "<cmd>MonokaiProSelect<cr>", desc = "Select Monokai pro filter" } },
+    "LazyVim/LazyVim",
     opts = {
-      transparent_background = false,
-      devicons = true,
-      filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-      day_night = {
-        enable = false,
-        day_filter = "pro",
-        night_filter = "spectrum",
-      },
-      inc_search = "background", -- underline | background
-      background_clear = {
-        "nvim-tree",
-        -- "neo-tree",
-        -- "bufferline",
-        "telescope",
-        "toggleterm",
-      },
-      plugins = {
-        bufferline = {
-          underline_selected = true,
-          underline_visible = false,
-          underline_fill = true,
-          bold = false,
-        },
-        indent_blankline = {
-          context_highlight = "pro", -- default | pro
-          context_start_underline = true,
-        },
-      },
-      override = function(c)
-        return {
-          -- ColorColumn = { bg = c.editor.background },
-          -- Mine
-          DashboardRecent = { fg = c.base.magenta },
-          DashboardProject = { fg = c.base.blue },
-          DashboardConfiguration = { fg = c.base.white },
-          DashboardSession = { fg = c.base.green },
-          DashboardLazy = { fg = c.base.cyan },
-          DashboardServer = { fg = c.base.yellow },
-          DashboardQuit = { fg = c.base.red },
-          IndentBlanklineChar = { fg = c.base.dimmed4 },
-          NeoTreeStatusLine = { link = "StatusLine" },
-          -- mini.hipatterns
-          MiniHipatternsFixme = { fg = c.base.black, bg = c.base.red, bold = true }, -- FIXME
-          MiniHipatternsTodo = { fg = c.base.black, bg = c.base.blue, bold = true }, -- TODO
-          MiniHipatternsHack = { fg = c.base.black, bg = c.base.yellow, bold = true }, -- HACK
-          MiniHipatternsNote = { fg = c.base.black, bg = c.base.green, bold = true }, -- NOTE
-          MiniHipatternsWip = { fg = c.base.black, bg = c.base.cyan, bold = true }, -- WIP
-        }
-      end,
-      overridePalette = function()
-        -- if filter == "pro" then
-        --   return {
-        --     dark2 = "#101014",
-        --     dark1 = "#16161E",
-        --     background = "#1A1B26",
-        --     text = "#C0CAF5",
-        --     accent1 = "#f7768e",
-        --     accent2 = "#7aa2f7",
-        --     accent3 = "#e0af68",
-        --     accent4 = "#9ece6a",
-        --     accent5 = "#0DB9D7",
-        --     accent6 = "#9d7cd8",
-        --     dimmed1 = "#737aa2",
-        --     dimmed2 = "#787c99",
-        --     dimmed3 = "#363b54",
-        --     dimmed4 = "#363b54",
-        --     dimmed5 = "#16161e",
-        --   }
-        -- end
-      end,
+      -- Use a built-in base and then fully override highlights in plain Lua.
+      colorscheme = "habamax",
     },
+    init = function()
+      local p = {
+        bg = "#1a1d2b",
+        fg = "#e6e6ff",
+        cursor = "#ffb86c",
+        sel_bg = "#2b3147",
+        sel_fg = "#f8f8f2",
+        c0 = "#1a1d2b",
+        c1 = "#ff6e6e",
+        c2 = "#9fe870",
+        c3 = "#ffd479",
+        c4 = "#7aa2ff",
+        c5 = "#bd93f9",
+        c6 = "#63d4e6",
+        c7 = "#d9dcff",
+        c8 = "#4b5270",
+        c9 = "#ff8a8a",
+        c10 = "#b6f58f",
+        c11 = "#ffe29a",
+        c12 = "#9bbcff",
+        c13 = "#d3b3ff",
+        c14 = "#86e6f5",
+        c15 = "#f8f8f2",
+      }
+
+      local function apply_custom_palette()
+        local set = vim.api.nvim_set_hl
+
+        -- Core UI
+        set(0, "Normal", { fg = p.fg, bg = p.bg })
+        set(0, "NormalFloat", { fg = p.fg, bg = p.bg })
+        set(0, "SignColumn", { bg = p.bg })
+        set(0, "LineNr", { fg = p.c8, bg = p.bg })
+        set(0, "CursorLineNr", { fg = p.c3, bold = true })
+        set(0, "CursorLine", { bg = p.sel_bg })
+        set(0, "CursorColumn", { bg = p.sel_bg })
+        set(0, "ColorColumn", { bg = p.sel_bg })
+        set(0, "Visual", { fg = p.sel_fg, bg = p.sel_bg })
+        set(0, "Cursor", { fg = p.bg, bg = p.cursor })
+        set(0, "Pmenu", { fg = p.c7, bg = p.bg })
+        set(0, "PmenuSel", { fg = p.c15, bg = p.sel_bg, bold = true })
+        set(0, "Search", { fg = p.bg, bg = p.c3 })
+        set(0, "IncSearch", { fg = p.bg, bg = p.c9, bold = true })
+        set(0, "MatchParen", { fg = p.c15, bg = p.c8, bold = true })
+        set(0, "StatusLine", { fg = p.c15, bg = p.sel_bg, bold = true })
+        set(0, "StatusLineNC", { fg = p.c8, bg = p.bg })
+        set(0, "WinBar", { fg = p.c7, bg = p.bg, bold = true })
+        set(0, "WinBarNC", { fg = p.c8, bg = p.bg })
+        set(0, "TabLine", { fg = p.c7, bg = p.bg })
+        set(0, "TabLineFill", { fg = p.c8, bg = p.bg })
+        set(0, "TabLineSel", { fg = p.c15, bg = p.sel_bg, bold = true })
+        set(0, "VertSplit", { fg = p.sel_bg, bg = p.bg })
+
+        -- Lualine defaults used by many setups
+        set(0, "lualine_a_normal", { fg = p.bg, bg = p.c4, bold = true })
+        set(0, "lualine_b_normal", { fg = p.c15, bg = p.sel_bg })
+        set(0, "lualine_c_normal", { fg = p.c7, bg = p.bg })
+        set(0, "lualine_a_insert", { fg = p.bg, bg = p.c2, bold = true })
+        set(0, "lualine_a_visual", { fg = p.bg, bg = p.c5, bold = true })
+        set(0, "lualine_a_replace", { fg = p.bg, bg = p.c1, bold = true })
+        set(0, "lualine_a_command", { fg = p.bg, bg = p.c3, bold = true })
+
+        -- Classic syntax groups
+        set(0, "Comment", { fg = p.c8, italic = true })
+        set(0, "String", { fg = p.c10 })
+        set(0, "Character", { fg = p.c2 })
+        set(0, "Number", { fg = p.c11 })
+        set(0, "Boolean", { fg = p.c9, bold = true })
+        set(0, "Float", { fg = p.c11 })
+        set(0, "Identifier", { fg = p.c12 })
+        set(0, "Function", { fg = p.c4, bold = true })
+        set(0, "Keyword", { fg = p.c13, italic = true })
+        set(0, "Conditional", { fg = p.c5, italic = true })
+        set(0, "Repeat", { fg = p.c5, italic = true })
+        set(0, "Operator", { fg = p.c14 })
+        set(0, "Type", { fg = p.c6 })
+        set(0, "PreProc", { fg = p.c3 })
+        set(0, "Constant", { fg = p.c9 })
+        set(0, "Special", { fg = p.c14 })
+        set(0, "Todo", { fg = p.bg, bg = p.c3, bold = true })
+        set(0, "Error", { fg = p.c1, bold = true })
+        set(0, "WarningMsg", { fg = p.c3 })
+
+        -- Treesitter and semantic highlights
+        set(0, "@comment", { fg = p.c8, italic = true })
+        set(0, "@string", { fg = p.c10 })
+        set(0, "@string.escape", { fg = p.c14 })
+        set(0, "@number", { fg = p.c11 })
+        set(0, "@boolean", { fg = p.c9, bold = true })
+        set(0, "@keyword", { fg = p.c13, italic = true })
+        set(0, "@keyword.return", { fg = p.c5, italic = true })
+        set(0, "@keyword.function", { fg = p.c5, italic = true })
+        set(0, "@operator", { fg = p.c14 })
+        set(0, "@type", { fg = p.c6 })
+        set(0, "@type.builtin", { fg = p.c6, bold = true })
+        set(0, "@function", { fg = p.c4, bold = true })
+        set(0, "@function.method", { fg = p.c12, bold = true })
+        set(0, "@constructor", { fg = p.c12, bold = true })
+        set(0, "@variable.parameter", { fg = p.c7 })
+        set(0, "@variable.member", { fg = p.c12 })
+        set(0, "@property", { fg = p.c12 })
+        set(0, "@constant", { fg = p.c9 })
+        set(0, "@module", { fg = p.c6 })
+
+        -- Lua-specific
+        set(0, "@constructor.lua", { fg = p.c6, bold = true })
+        set(0, "@keyword.lua", { fg = p.c13, italic = true })
+        set(0, "@function.builtin.lua", { fg = p.c4, bold = true })
+
+        -- TS/JS/TSX-specific
+        set(0, "@property.typescript", { fg = p.c12 })
+        set(0, "@property.tsx", { fg = p.c12 })
+        set(0, "@variable.member.typescript", { fg = p.c12 })
+        set(0, "@variable.member.tsx", { fg = p.c12 })
+        set(0, "@tag", { fg = p.c4 })
+        set(0, "@tag.attribute", { fg = p.c6 })
+
+        -- Rust-specific
+        set(0, "@type.rust", { fg = p.c6, bold = true })
+        set(0, "@type.qualifier.rust", { fg = p.c5, italic = true })
+        set(0, "@function.macro.rust", { fg = p.c14, bold = true })
+        set(0, "@lsp.type.lifetime.rust", { fg = p.c3, italic = true })
+        set(0, "@lsp.type.selfKeyword.rust", { fg = p.c13, italic = true })
+
+        -- Diagnostics and git diff
+        set(0, "DiagnosticError", { fg = p.c1 })
+        set(0, "DiagnosticWarn", { fg = p.c3 })
+        set(0, "DiagnosticInfo", { fg = p.c4 })
+        set(0, "DiagnosticHint", { fg = p.c6 })
+        set(0, "DiffAdd", { fg = p.c2 })
+        set(0, "DiffChange", { fg = p.c4 })
+        set(0, "DiffDelete", { fg = p.c1 })
+        set(0, "DiffText", { fg = p.c15, bg = p.c8 })
+
+        -- Keep your custom dashboard + plugin groups
+        set(0, "DashboardRecent", { fg = p.c5 })
+        set(0, "DashboardProject", { fg = p.c4 })
+        set(0, "DashboardConfiguration", { fg = p.fg })
+        set(0, "DashboardSession", { fg = p.c2 })
+        set(0, "DashboardLazy", { fg = p.c6 })
+        set(0, "DashboardServer", { fg = p.c3 })
+        set(0, "DashboardQuit", { fg = p.c1 })
+        set(0, "IndentBlanklineChar", { fg = p.sel_bg })
+        set(0, "NeoTreeStatusLine", { link = "StatusLine" })
+
+        set(0, "MiniHipatternsFixme", { fg = p.c0, bg = p.c1, bold = true })
+        set(0, "MiniHipatternsTodo", { fg = p.c0, bg = p.c4, bold = true })
+        set(0, "MiniHipatternsHack", { fg = p.c0, bg = p.c3, bold = true })
+        set(0, "MiniHipatternsNote", { fg = p.c0, bg = p.c2, bold = true })
+        set(0, "MiniHipatternsWip", { fg = p.c0, bg = p.c6, bold = true })
+      end
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = apply_custom_palette,
+      })
+
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = apply_custom_palette,
+      })
+    end,
   },
 }
