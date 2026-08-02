5c3c3903dfaaadc545f62080261956ff04020fe9
Joshua Ross <git@rossjm.dev>
2026-08-01 20:40:28 -0400

    Update .config/zsh/conf.d/00-env.zsh Update .zshenv

--- home/dot_config/zsh/conf.d/00-env.zsh
+++ home/dot_config/zsh/conf.d/00-env.zsh
@@ -33,25 +33,6 @@ export INFISICAL_DOMAIN="https://infisical.jershlabs.xyz"
 # ---------- Starship ----------
 export STARSHIP_CONFIG="$ZDOTDIR/conf.d/starship.toml"
 
-# ---------- eza ----------
-export EZA_COLORS="\
-uu=36:\
-uR=31:\
-un=35:\
-gu=37:\
-da=2;34:\
-ur=34:\
-uw=95:\
-ux=36:\
-ue=36:\
-gr=34:\
-gw=35:\
-gx=36:\
-tr=34:\
-tw=35:\
-tx=36:\
-xx=95:"
-
 # ---------- PATH ----------
 # Personal binaries/scripts
 export PATH="$HOME/.local/bin:$PATH"
