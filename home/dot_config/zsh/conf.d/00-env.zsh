ed83521d3caaea9e45fb09874a7684f50d19764b
Joshua Ross <git@rossjm.dev>
2026-08-03 18:30:18 -0400

    Update .config/zsh/conf.d/00-env.zsh Remove .config/zsh/conf.d/40-eza.zsh

--- home/dot_config/zsh/conf.d/00-env.zsh
+++ home/dot_config/zsh/conf.d/00-env.zsh
@@ -55,3 +55,24 @@ setopt HIST_FIND_NO_DUPS
 setopt AUTOCD
 setopt NOBEEP
 setopt NUMERIC_GLOB_SORT  # sort file10 after file9, not after file1
+
+# ---------------------
+# eza universal Dracula
+# ---------------------
+export EZA_COLORS="\
+uu=36:\
+uR=31:\
+un=35:\
+gu=37:\
+da=2;34:\
+ur=34:\
+uw=95:\
+ux=36:\
+ue=36:\
+gr=34:\
+gw=35:\
+gx=36:\
+tr=34:\
+tw=35:\
+tx=36:\
+xx=95:"
