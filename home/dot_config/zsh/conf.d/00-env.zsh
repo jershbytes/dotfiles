129816d1c738f99495190cfa2dcc121026be97c1
Joshua Ross <git@rossjm.dev>
2026-08-03 18:36:11 -0400

    Update .config/zsh/conf.d/00-env.zsh Remove .config/zsh/conf.d/40-eza.zsh

--- home/dot_config/zsh/conf.d/00-env.zsh
+++ home/dot_config/zsh/conf.d/00-env.zsh
@@ -6,6 +6,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+export EZA_CONFIG_DIR="$XDG_CONFIG_HOME/eza"
 
 # ---------- Editor ----------
 # Default editor used by git, crontab, etc.
